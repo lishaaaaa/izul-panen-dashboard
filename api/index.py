f9ba03e71a4e73074a19e7c69225e58d6d922cb7
felisha dita <95285200+lishaaaaa@users.noreply.github.com>
2025-11-04 21:27:14 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,6 +1,6 @@
-# api/index.py
-from app import app
 from serverless_wsgi import handle_request
+from app import app as flask_app
 
-def handler(request, context):
-    return handle_request(app, request, context)
+# Vercel akan memanggil fungsi ini
+def handler(event, context):
+    return handle_request(flask_app, event, context)
