3fc5ef921b38e063faeb1b3b6f4278a59d337a3e
felisha dita <95285200+lishaaaaa@users.noreply.github.com>
2025-11-04 21:27:53 +0700

    Update ping.py

--- api/ping.py
+++ api/ping.py
@@ -3,3 +3,7 @@ def handler(request, context):
     return {"statusCode": 200, "body": "pong"}
 def handler(request, context):
     return {"statusCode": 200, "body": "pong"}
+def handler(request, response):
+    response.status_code = 200
+    response.send("pong")
+
