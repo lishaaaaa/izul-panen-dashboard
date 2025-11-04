7adb87edaa8a4d6a3ab1ccad595a30915632b42d
felisha dita <95285200+lishaaaaa@users.noreply.github.com>
2025-11-04 21:02:53 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,2 +1,6 @@
 # api/index.py
-from app import app  # re-export WSGI app untuk Vercel
+from app import app
+from serverless_wsgi import handle_request
+
+def handler(request, context):
+    return handle_request(app, request, context)
