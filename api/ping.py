85de630b06fe4fbfc51c2253f41574e0d687947a
felisha dita <95285200+lishaaaaa@users.noreply.github.com>
2025-11-04 21:58:20 +0700

    Delete api/ping.py

--- api/ping.py
+++
@@ -1,9 +0,0 @@
-# api/ping.py
-def handler(request, context):
-    return {"statusCode": 200, "body": "pong"}
-def handler(request, context):
-    return {"statusCode": 200, "body": "pong"}
-def handler(request, response):
-    response.status_code = 200
-    response.send("pong")
-
